6fcd3883e43e3b17d05737f2127bba804c353ec8
benkwee <benqxnguyen@gmail.com>
2018-02-21 22:43:03 -0800

    Add crime list fragment and singleton fragment activity

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Fragment;
+
+/**
+ * Created by ben on 2/21/18.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+}
