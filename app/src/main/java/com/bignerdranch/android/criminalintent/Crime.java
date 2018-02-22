63a065ab4d07a760af61bd8c820b77f3573183f5
benkwee <benqxnguyen@gmail.com>
2018-02-21 21:50:48 -0800

    Create crime fragment

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,53 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by ben on 2/21/18.
+ */
+
+public class Crime {
+
+  private UUID mID;
+  private String mTitle;
+  private Date mDate;
+  private Boolean mSolved;
+
+  public Crime() {
+    mID = UUID.randomUUID();
+    mDate = new Date();
+  }
+
+  public UUID getID() {
+    return mID;
+  }
+
+  public void setID(UUID ID) {
+    mID = ID;
+  }
+
+  public String getTitle() {
+    return mTitle;
+  }
+
+  public void setTitle(String title) {
+    mTitle = title;
+  }
+
+  public Date getDate() {
+    return mDate;
+  }
+
+  public void setDate(Date date) {
+    mDate = date;
+  }
+
+  public Boolean getSolved() {
+    return mSolved;
+  }
+
+  public void setSolved(Boolean solved) {
+    mSolved = solved;
+  }
+}
